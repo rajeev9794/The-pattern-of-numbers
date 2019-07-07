935d8fd259ba27887a83a69c90e7330305315090
RAJEEV MAURYA <43512770+rajeev9794@users.noreply.github.com>
2019-07-07 21:22:41 +0530

    Update patt3.cpp

--- patt3.cpp
+++ patt3.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 void odd(int n){
  int i,j,k,ch=0,c;
- for(i=1;i<=n;i++){
+ for(i=1;i<=n;i++)
+ {
    k=1,c=0;
   (i<=n/2+1?ch++:ch--);
-  for(j=1;j<=n;j++){
-     if(j>=n/2-ch+2&&j<=n/2+ch){
-      k=(j<=((n/2)+1)?++c:--c);
-      cout<<k;
+  for(j=1;j<=n;j++)
+  {
+     if(j>=n/2-ch+2&&j<=n/2+ch)
+     {
+       k=(j<=((n/2)+1)?++c:--c);
+       cout<<k;
       }
      else
-      cout<<'*';
-    }
-  cout<<endl;
+       cout<<'*';
+   }
+   cout<<endl;
    }
 }
 void even(int n){
